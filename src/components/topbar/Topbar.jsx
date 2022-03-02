5c8b553a67e938c28cc2c3d9bce9b098e5c3a0d9
Aden Adhanom <adenadhanom@Adens-MacBook-Air.local>
2022-03-02 16:10:54 -0500

    i made some chages to all the components

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,8 +2,14 @@ import "./topbar.scss"
 
 export default function Topbar() {
     return (
-        <div className="topbar">
-            
+        <div className="topbar" id="topbar">
+            <div className="wrapper">
+            <div className="left">
+                <a href="#intro">jhasd</a>
+
+            </div>
+            <div className="right"></div>
+            </div>
         </div>
     )
 }
