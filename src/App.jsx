b5c4d68d89f35a07efa5c014e8614f01342e8b37
Aden Adhanom <adenadhanom@Adens-MacBook-Air.local>
2022-03-03 11:10:47 -0500

    starting to create the menu down drop

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,15 @@ import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import "./app.scss"
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
+
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
