2e393a23d8bb0d7ff39d9fe3fe9978063de77869
Aden Adhanom <adenadhanom@Adens-MacBook-Air.local>
2022-03-03 10:07:08 -0500

    topbar styling is almost done

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,14 +1,30 @@
-import "./topbar.scss"
+import "./topbar.scss";
+import {Person,Mail} from "@material-ui/icons";
+
 
 export default function Topbar() {
     return (
-        <div className="topbar" id="topbar">
+        <div className="topbar">
             <div className="wrapper">
             <div className="left">
-                <a href="#intro">jhasd</a>
-
+                <a href="#intro" className="logo">genius.</a>
+                <div className="itemContainer">
+                    <Person className="icon" />
+                    <span>317 384 2167</span>  
+                </div>
+                <div className="itemContainer">
+                    <Mail className="icon" />
+                    <span>samghagos@gmail.com</span>  
+                    
+                </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="hamburger">
+                    <span className="line1"></span>
+                    <span className="line2"></span>
+                    <span className="line3"></span>
+                </div>
+            </div>
             </div>
         </div>
     )
