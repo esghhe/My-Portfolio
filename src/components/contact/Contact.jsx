89143ff2b7ad51e3693340e9ca622bf5a518578d
Aden Adhanom <adenadhanom@Adens-MacBook-Air.local>
2022-03-04 20:32:30 -0500

    Testimonials page is finished too

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,8 @@ import "./contact.scss"
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-            
+            <div className="left"></div>
+            <div className="right"></div>
         </div>
     )
 }
