2dca62d1ce45a3a2143da5063f8e62b1ad06ca20
Aden Adhanom <adenadhanom@Adens-MacBook-Air.local>
2022-03-05 09:36:21 -0500

    finalizing the readme file

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -11,7 +11,7 @@ export default function Works() {
             desc:
               "",
             img:
-              "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+              "./assets/Flight-Planner.png",
           },
           {
             id: "2",
